93af030f4d8ec8fece773339319ff2acaa1c2b93
AaronKolenda <kolenda.a@gmail.com>
2015-04-21 10:39:25 -0400

    adding readme, cleaning up code, commenting

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a recreation of the main section of the old ESPN homepage. The point of this project was to use Angular.js to get the data via an api, and display it on the page. Using css, I've recreated the exact look of the original website, and even made it more responsive than the actual ESPN site. 
--- index.js
+++ index.js
@@ -6,18 +6,42 @@ sportsApp.controller("CarouselController", [ "$scope", "getData", function($scop
 
     $scope.data = data
 
+      _.each($scope.data, function(element) {
+
+          _.each(element.big.links, function(e) {   //set the appropiate glyph
+      if (e.glyph) {
+        if (e.glyph === "video") {
+          e.glyph = "http://assets.espn.go.com/icons/watch_headlines.png"
+        }
+        if (e.glyph === "insider") {
+          e.glyph = "http://a.espncdn.com/icons/in.gif"
+        }
+        if (e.glyph === "live") {
+          e.glyph = "http://assets.espn.go.com/i/ls.gif"
+        }
+      }
+      });
+      
+    });
+
   });
 
   $scope.hover = false;
+  $scope.index = 0;
+
+  //The following is for managing the hover functionality with more complexity than using a simple ng-hover
 
   $scope.startHover = function(e) {
+    console.log(e);
     $scope.hover = true;
-    angular.element(e.srcElement).css("bottom", "12px");
   }
 
   $scope.stopHover = function(e) {
     $scope.hover = false;
-    angular.element(e.srcElement).css("bottom", "2px");
+  }
+
+  $scope.showBig = function(index) {
+    $scope.index = index;
   }
 
 }]);
@@ -36,25 +60,26 @@ sportsApp.controller("HighlightsController", [ "$scope", "getData", function($sc
 
   getData("http://tiy-espn-info.herokuapp.com/espn/headlines", function(data){
 
-  	console.log(data);
     $scope.data = data
 
-    _.each($scope.data, function(element, index) {
-    	if (element.glyph) {
-    		if (element.glyph === "video") {
-    			element.glyph = "http://assets.espn.go.com/icons/watch_headlines.png"
-    		}
-    		if (element.glyph === "insider") {
-    			element.glyph = "http://a.espncdn.com/icons/in.gif"
-    		}
-    		if (element.glyph === "live") {
-    			element.glyph = "http://assets.espn.go.com/i/ls.gif"
-    		}
-    	}
+    _.each($scope.data, function(element) {   // Set the appropiate glyph
+      if (element.glyph) {
+        if (element.glyph === "video") {
+          element.glyph = "http://assets.espn.go.com/icons/watch_headlines.png"
+        }
+        if (element.glyph === "insider") {
+          element.glyph = "http://a.espncdn.com/icons/in.gif"
+        }
+        if (element.glyph === "live") {
+          element.glyph = "http://assets.espn.go.com/i/ls.gif"
+        }
+      }
       
     });
 
-      _.each($scope.data, function(element, index) {
+    // This is adding a thin grey border to only the highlights that have an analysis.
+
+      _.each($scope.data, function(element) {
         element.border = false;
         if (element.analysis) {
           if (element.analysis.text.replace(/ /g,'') !== "") {
@@ -62,12 +87,8 @@ sportsApp.controller("HighlightsController", [ "$scope", "getData", function($sc
           }
         }
       });
-      console.log($scope.data);
-
 
   });
-
-
 
 
 }]);
